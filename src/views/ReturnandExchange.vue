f2bd1b7ae6b56eb68b999683263c62fad85d27ca
LIUYAXIN\32420 <324097807>
2025-07-03 23:15:51 +0800

    文件样式修改新增

--- src/views/ReturnandExchange.vue
+++ src/views/ReturnandExchange.vue
@@ -3,7 +3,241 @@
   <div class="home-page">
     <blacknav></blacknav>
     <whitenav></whitenav>
-
+    <div class="breadcrumbs">
+      <div class="container"><a href="https://www.mi.com/">首页</a> <span class="sep">/</span> <a
+          href="https://www.mi.com/service/">服务中心</a> <span class="sep">/</span> <span>小米售后服务政策查询</span></div>
+    </div>
+    <div class="mi-exchange">
+      <div class="container">
+        <img src="../assets/img/exchange.jpg" >
+        <div class="mi-promise">
+          <h2 class="webfont">小米承诺</h2>
+          <div class="content">
+            <div>
+              <ul class="clearfix">
+                <li><span class="icon icon01"></span> <span class="webfont">7日内无理由退货</span></li>
+                <li><span class="icon icon02"></span> <span class="webfont">质量问题15日内包换</span></li>
+                <li><span class="icon icon03"></span> <span class="webfont">1年质保</span></li>
+              </ul>
+              <p>如因产品质量问题，凭厂商授权服务中心的检测证明，享受7日内退货，手机类15天换货、电视30日内换货，在质保期内享受1年内免费维修等三包服务！<br>（注：部分商品由厂家提供售后，或者服务周期存在日期差异，具体详则请查看对应品类的售后政策说明）
+              </p>
+            </div>
+          </div>
+        </div>
+        <div  class="mi-exchange-box__wrapper">
+          <div  class="mi-exchange-box">
+            <h2 >产品售后政策查询</h2>
+            <div  class="content clearfix">
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#1"target="_blank">骑行运动及相关</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#2"target="_blank">路由及WIFI</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#3" target="_blank">服装配饰</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#4" target="_blank">笔记本</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#5" target="_blank">厨卫电器</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#7" target="_blank">生活电器</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#9"target="_blank">打印机＆无人机＆耗材</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#10"target="_blank">VR眼镜</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#11"target="_blank">插座及开关控制</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#13"target="_blank">智能灯具</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#15"target="_blank">家居日用</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#16"target="_blank">维修工具</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#17"target="_blank">影音娱乐</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#22"target="_blank">儿童产品</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#23"target="_blank">摄影摄像门铃</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#24"target="_blank">车载电子产品</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#25" target="_blank">可穿戴</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#26"target="_blank">鼠标＆键盘</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#27"target="_blank">个护健康</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#28"target="_blank">数据线及充电</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#30" target="_blank">自拍杆</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#32" target="_blank">电视</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#34"target="_blank">手机＆平板</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#35"target="_blank">贴纸&amp;保护套&amp;卡针</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#37" target="_blank">投影仪</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#39"target="_blank">电视周边</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#40"target="_blank">检测报警产品</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#41"target="_blank">米家空调</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#44"target="_blank">米家新风机</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#45"target="_blank">智能门锁</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#46"target="_blank">米家洗衣机</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#47" target="_blank">翻译机</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#48"target="_blank">米家冰箱</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#49" target="_blank">显示器</a>
+                </dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#50"target="_blank">小米有品商品</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#52"target="_blank">米家冰柜</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#53"target="_blank">迷你主机</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#54"target="_blank">米家中央空调</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#55"target="_blank">汽车周边产品</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#56"target="_blank">米家多联机</a></dd>
+              </dl>
+              <dl >
+                <dt ></dt>
+                <dd ><a href="#57"target="_blank">空调周边</a></dd>
+              </dl>
+            </div>
+          </div>
+          <div  class="mi-exchange-box exchange-box">
+            <div  class="content">
+              <div  class="clearfix">
+                <dl >
+                  <dd >
+                    产品保外维修物料价格查询
+                    <a  href="https://www.mi.com/service/materialprice/"
+                       target="_blank">了解详情&gt;</a>
+                  </dd>
+                  <dd >
+                    停止服务产品公告查询
+                    <a 
+                       href="http://www.mi.com/a/h/20805.html?sign=46d0249fae8f2ea8102c0b19e699bdde"
+                       target="_blank">了解详情&gt;&gt;</a>
+                  </dd>
+                  <dd >
+                    以换代修产品保外售后规则
+                    <a 
+                       href="https://m.mi.com/mfbs/dghd/m_universal_non_top?_rt=rn&amp;pageid=12768&amp;pdl=mishop&amp;sign=425b597a086556f49b99dd43df725ed9"
+                       target="_blank">了解详情&gt;&gt;</a>
+                  </dd>
+                  <dd >
+                    汽车智能泊车辅助OTA升级召回公告
+                    <a 
+                       href="https://company.cnbj1.mi-fds.com/company/info/news-1.html"
+                       target="_blank">了解详情&gt;&gt;</a>
+                  </dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   <myfooter></myfooter>
 </template>
@@ -42,8 +276,143 @@ export default {
 </script>
 
 <style scoped>
+
 .home-page {
   min-width: 1226px;
   position: relative;
 }
+.breadcrumbs {
+  height: 40px;
+  font-size: 12px;
+  line-height: 40px;
+  background: #f5f5f5
+}
+
+.breadcrumbs,.breadcrumbs a {
+  color: #757575
+}
+.container {
+  width: 1226px;
+  margin-right: auto;
+  margin-left: auto
+}
+
+.container:after,.container:before {
+  content: " ";
+  display: table
+}
+
+.container:after {
+  clear: both
+}
+.mi-exchange {
+  background: #f5f5f5
+}
+
+.mi-exchange img {
+  display: block
+}
+
+.mi-exchange h2 {
+  display: block;
+  font-size: 30px;
+  margin-top: 30px;
+  color: #666;
+  font-weight: 400;
+  text-align: center;
+  line-height: 106px
+}
+
+.mi-exchange .mi-promise p {
+  padding: 35px 0;
+  text-align: center;
+  color: #616161;
+  font-size: 14px;
+  border-top: 1px solid #f2f2f2
+}
+
+.mi-exchange .mi-promise .content {
+  background: #fff
+}
+
+.mi-exchange .mi-promise .content div {
+  margin: 0 62px
+}
+
+.mi-exchange .mi-promise ul {
+  text-align: center
+}
+
+.mi-exchange .mi-promise li {
+  list-style: none;
+  display: inline-block;
+  margin: 67px 80px 62px
+}
+
+.mi-exchange .mi-promise li .icon {
+  width: 97px;
+  height: 97px;
+  margin-bottom: 24px;
+  background: url(//c1.mifile.cn/f/i/17/service/exchangeicon.png?12=) 0 50% no-repeat
+}
+
+.mi-exchange .mi-promise li .icon01 {
+  background-position: 0 50%
+}
+
+.mi-exchange .mi-promise li .icon02 {
+  background-position: -100px 50%
+}
+
+.mi-exchange .mi-promise li .icon03 {
+  background-position: -200px 50%
+}
+
+.mi-exchange .mi-promise li span {
+  display: block;
+  margin: 0 auto;
+  font-size: 14px
+}
+.clearfix:after,.clearfix:before {
+  content: " ";
+  display: table
+}
+
+.clearfix:after {
+  clear: both
+}
+
+.mi-exchange-box__wrapper .mi-exchange-box {
+  margin-bottom: 45px
+}
+
+.mi-exchange-box__wrapper .mi-exchange-box .content {
+  padding: 28px 0;
+  background: #fff;
+}
+
+.mi-exchange-box__wrapper .mi-exchange-box dl {
+  margin: 0;
+  width: 206px;
+  float: left;
+  padding: 14px 50px;
+  border: 0
+}
+
+.mi-exchange-box__wrapper .mi-exchange-box dt {
+  font-size: 18px;
+  line-height: 36px;
+  color: #333;
+  padding-bottom: 5px
+}
+
+.mi-exchange-box__wrapper .exchange-box dl {
+  width: auto;
+  display: block
+}
+
+.mi-exchange-box__wrapper .exchange-box a {
+  color: #ff6700
+}
+
 </style>
